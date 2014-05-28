4b8756d359027cb18f8e3af664f70e41cc39c569
froehr <f.roehr@uni-muenster.de>
2014-05-28 16:59:27 +0200

    New Databasestructure and Function

--- Database/database.sql
+++ Database/database.sql
@@ -1,55 +1,62 @@
-DROP TABLE interval;
-DROP TABLE seasonal;
-DROP TABLE event;
+DROP TABLE closed;
+DROP TABLE opened;
+DROP TABLE temporal_event;
 DROP TABLE pub;
-DROP FUNCTION is_open(time);
+DROP FUNCTION is_open(p_id integer, ts timestamp);
 
 CREATE TABLE pub(
 pub_id SERIAL NOT NULL,
 PRIMARY KEY (pub_id)
 );
 
-CREATE TABLE event(
+CREATE TABLE temporal_event(
 event_id SERIAL NOT NULL,
 pub_id SERIAL NOT NULL,
 name TEXT NOT NULL,
-open BOOLEAN,
+description TEXT,
 PRIMARY KEY (event_id),
 FOREIGN KEY (pub_id) REFERENCES pub
 	ON DELETE CASCADE
 	ON UPDATE CASCADE
 );
 
-CREATE TABLE interval(
-interval_id SERIAL NOT NULL,
+CREATE TABLE closed(
+closed_id SERIAL NOT NULL,
+event_id SERIAL NOT NULL,
+start_closed TIMESTAMP,
+end_closed TIMESTAMP,
+PRIMARY KEY (closed_id),
+FOREIGN KEY (event_id) REFERENCES temporal_event
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+CREATE TABLE opened(
+opened_id SERIAL NOT NULL,
 event_id SERIAL NOT NULL,
 periodic TEXT,
-start_interval TIMESTAMP,
-end_interval TIMESTAMP,
-PRIMARY KEY (interval_id),
-FOREIGN KEY (event_id) REFERENCES event
+occurences INTEGER,
+start_opened TIMESTAMP,
+end_opened TIMESTAMP,
+PRIMARY KEY (opened_id),
+FOREIGN KEY (event_id) REFERENCES temporal_event
 	ON DELETE CASCADE
 	ON UPDATE CASCADE
 );
 
-CREATE TABLE seasonal(
-seasonal_id SERIAL NOT NULL,
-event_id SERIAL NOT NULL,
-start_seasonal TIMESTAMP,
-end_seasonal TIMESTAMP,
-PRIMARY KEY (seasonal_id),
-FOREIGN KEY (event_id) REFERENCES event
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
-);
-
-CREATE FUNCTION is_open(time) RETURNS BOOLEAN AS '
+-- Test: SELECT is_open(5,'2001-02-16');
+CREATE FUNCTION is_open(p_id integer, ts timestamp) RETURNS BOOLEAN AS $$
+	DECLARE result BOOLEAN := FALSE;
+	DECLARE number integer;
 	BEGIN
-	  IF current_timestamp BETWEEN interval.start_interval and interval.end_interval && current_timestamp BETWEEN seasonal.start_seasonal and seasonal.start_seasonal
-	  THEN
-		RETURN TRUE;
-	  ELSE
-		RETURN FALSE;
-	  END IF;
+	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN opened WHERE pub.pub_id = p_id AND (ts BETWEEN opened.start_opened AND opened.end_opened);
+	 IF number = 0
+	    THEN result := TRUE;
+	 END IF;
+	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN closed WHERE pub.pub_id = p_id AND (ts BETWEEN closed.start_closed AND closed.end_closed);
+	 IF number = 0
+		THEN result := FALSE;
+	 END IF;
+	 RETURN result;
 	END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
