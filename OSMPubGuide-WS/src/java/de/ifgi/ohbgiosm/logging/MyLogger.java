9aaf64a61ccba99067064d8e4d4e35a7f732a0da
ChristopherStephan <c.stephan@wwu.de>
2014-06-30 08:37:30 +0200

    Logger and database connection added

---
+++ OSMPubGuide-WS/src/java/de/ifgi/ohbgiosm/logging/MyLogger.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.ifgi.ohbgiosm.logging;
+
+import org.apache.log4j.*;
+
+/**
+ *
+ * @author christopher
+ */
+public class MyLogger {
+
+    private static MyLogger instance = null;
+    private Logger logger;
+
+    private MyLogger() {
+        logger = Logger.getRootLogger();
+        initLogger(logger);
+    }
+
+    public static MyLogger getInstance() {
+        if (instance == null) {
+            instance = new MyLogger();
+        }
+        return instance;
+    }
+
+    public Logger getLogger() {
+        return this.logger;
+    }
+
+    private void initLogger(Logger logger) {
+        try {
+
+            PatternLayout layout = new PatternLayout("%d{ISO8601} %-5p [%t] %c: %m%n");
+
+            ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+            logger.addAppender(consoleAppender);
+
+            DailyRollingFileAppender fileAppender = new DailyRollingFileAppender(layout, "logs/logs.log", "'.'yyyy-MM-dd_HH-mm");
+            logger.addAppender(fileAppender);
+
+            logger.setLevel(Level.ALL);
+
+        } catch (Exception ex) {
+            logger.error(ex);
+        }
+    }
+}
