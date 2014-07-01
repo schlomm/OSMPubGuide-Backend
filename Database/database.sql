689b2eefc54edf250c5fa9e92bc03526a28ade95
Andreas Ohrem <a_ohre01@uni-muenster.de>
2014-07-01 09:27:42 +0200

    Delete periodic function in database

--- Database/database.sql
+++ Database/database.sql
@@ -4,6 +4,7 @@ DROP TABLE temporal_event;
 DROP TABLE pub;
 DROP FUNCTION is_open(p_id integer, ts timestamp);
 DROP FUNCTION insert_periodic(event_id integer, state TEXT, start_date timestamp, end_date timestamp, end_periodic timestamp, periodic integer);
+DROP FUNCTION delete_periodic(event_id integer, start_date timestamp, end_date timestamp, end_periodic timestamp, periodic integer);
 
 CREATE TABLE pub(
 pub_id SERIAL NOT NULL,
@@ -45,6 +46,12 @@ FOREIGN KEY (event_id) REFERENCES temporal_event
 	ON UPDATE CASCADE
 );
 
+/*
+	Function determines if a certain pub is opened at a certain time
+	Param: p_id			INTEGER			OSM Pub ID of the pub
+	Param: ts			TIMESTAMP		Checks if the pub is opened at this time
+	Returns: true: pub is opened, false: pub is closed
+*/
 CREATE FUNCTION is_open(p_id integer, ts timestamp) RETURNS BOOLEAN AS $$
 	DECLARE result BOOLEAN := FALSE;
 	DECLARE number integer;
@@ -61,6 +68,15 @@ CREATE FUNCTION is_open(p_id integer, ts timestamp) RETURNS BOOLEAN AS $$
 	END;
 $$ LANGUAGE 'plpgsql';
 
+/*
+	Function creates multiple entries in the "closed" respectively the "opened"-table
+	Param: event_id			INTEGER 		ID of the event for which the periodic opening/closed hours shall be created
+	Param: state			TEXT 			can be 'opened' or 'closed', decides which table will be used
+	Param: start_date		TIMESTAMP 		Start date of the first occurrence of opening hour/closed hour
+	Param: end_date			TIMESTAMP		End date of the first occurrence of opening hour/closed hour
+	Param: end_periodic		TIMESTAMP		Last occurrence of the periodic event
+	Param: periodic			INTEGER			Timespan between end_periodic and next start of the event
+*/
 CREATE FUNCTION insert_periodic(event_id integer, state TEXT, start_date timestamp, end_date timestamp, end_periodic timestamp, periodic integer) RETURNS void AS $$
     DECLARE start1 integer := EXTRACT(EPOCH FROM start_date);
     DECLARE end1 integer := EXTRACT(EPOCH FROM end_date);
@@ -82,4 +98,25 @@ BEGIN
 END 
 $$ LANGUAGE 'plpgsql';
 
-
+/*
+	Function deletes multiple entries from "closed" and the "opened"-table
+	Param: e_id				INTEGER 		ID of the event for which the periodic opening/closed hours shall be deleted
+	Param: start_date		TIMESTAMP 		Start date of the first occurrence of opening hour/closed hour
+	Param: end_date			TIMESTAMP		End date of the first occurrence of opening hour/closed hour
+	Param: end_periodic		TIMESTAMP		Last occurrence of the periodic event
+	Param: periodic			INTEGER			Timespan between end_periodic and next start of the event
+*/
+CREATE FUNCTION delete_periodic(e_id integer, start_date timestamp, end_date timestamp, end_periodic timestamp, periodic integer) RETURNS void AS $$
+    DECLARE start1 integer := EXTRACT(EPOCH FROM start_date);
+    DECLARE end1 integer := EXTRACT(EPOCH FROM end_date);
+    DECLARE end_periodic INTEGER := EXTRACT(EPOCH FROM end_periodic); 
+BEGIN
+    WHILE (start1 <= end_periodic) 
+    LOOP
+		DELETE FROM closed WHERE event_id = e_id AND start_time = to_timestamp(start1) AND end_time = to_timestamp(end1);
+		DELETE FROM opened WHERE event_id = e_id AND start_time = to_timestamp(start1) AND end_time = to_timestamp(end1);
+		start1 := start1 + periodic;
+		end1 := end1 + periodic;
+    END LOOP;
+END 
+$$ LANGUAGE 'plpgsql';
