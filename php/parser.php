658cc1de3f9b90c7beeda077159b4ff4c4ab7098
Andreas Ohrem <a_ohre01@uni-muenster.de>
2014-07-01 09:58:26 +0200

    Continuing work on parser

--- php/parser.php
+++ php/parser.php
@@ -2,7 +2,7 @@
 	$osm_api_link = 'http://api.openstreetmap.org/api/0.6/';
 	
 	// OSM node IDs of all bars and pubs of our system
-	$osm_pub_ids = array(439279333, 271428122);
+	$osm_pub_ids = array(439279333/*, 271428122*/);
 	
 	// Iterate all OSM nodes from the array and parse opening hours
 	foreach ( $osm_pub_ids as $osm_node_id ) {
@@ -24,8 +24,31 @@
 			}
 		}
 		
+		$opening_hours = 'Mo 10:00-12:00,12:30-15:00; Tu-Fr 08:00-12:00,12:30-15:00; Sa 08:00-12:00';
+		
 		if ( $opening_hours != null ) {
-			print $opening_hours;
+			$days = explode(';', $opening_hours);
+			
+			foreach ( $days as $day ) {
+				$times = explode(',', $day);
+				$week_day = '';
+				$opening_times = array();
+				
+				foreach($times as $time) {
+					$time = trim($time);
+					
+					if ( $week_day == '' ) {
+						$week_day = explode(' ', $time)[0];
+						$opening_times[] = explode(' ', $time)[1];
+					}
+					else {
+						$opening_times[] = $time;
+					}
+				}
+				
+				print $week_day;
+				print_r($opening_times);
+			}
 		}
 	}
 ?>
