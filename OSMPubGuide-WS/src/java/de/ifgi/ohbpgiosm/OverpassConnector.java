990dbb574f90628b168801c4d51f6cb634dee381
zvaraondrej <zvara.ondrej@gmail.com>
2014-07-04 15:07:22 +0200

    OverpassConnector union bars, nightclubs and pubs

--- OSMPubGuide-WS/src/java/de/ifgi/ohbpgiosm/OverpassConnector.java
+++ OSMPubGuide-WS/src/java/de/ifgi/ohbpgiosm/OverpassConnector.java
@@ -7,6 +7,7 @@ package de.ifgi.ohbpgiosm;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -18,6 +19,7 @@ import noNamespace.OsmDocument;
 import noNamespace.OsmScriptDocument;
 import noNamespace.OsmScriptType;
 import noNamespace.QueryOSMType;
+import noNamespace.UnionType;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlOptions;
 import org.w3c.dom.Document;
@@ -30,6 +32,7 @@ import org.xml.sax.SAXException;
 public class OverpassConnector extends Connector {
 
     private String hostName = "http://overpass-api.de/api/interpreter";
+    private List<String> amenities = Arrays.asList("pub", "nightclub", "bar");
 
     /**
      * Constructor
@@ -50,11 +53,15 @@ public class OverpassConnector extends Connector {
         opts.setCharacterEncoding("UTF-8");
 
         OsmScriptDocument osd = OsmScriptDocument.Factory.newInstance(opts); //the document
-        OsmScriptType ost = osd.addNewOsmScript(); 
-        
+        OsmScriptType ost = osd.addNewOsmScript();
+        UnionType union = ost.addNewUnion();
+
         Document response;
         try {
-            composeOsmScript(ost);
+            for (String amenity : amenities) {
+                composeOsmScript(union, amenity);
+            }
+            ost.addNewPrint();
             response = HttpClient.getInstance().sendPostRequest(this.hostName, osd.toString());
             this.response = OsmDocument.Factory.parse(response);
         } catch (IOException ex) {
@@ -76,14 +83,13 @@ public class OverpassConnector extends Connector {
      * @param ost OsmScriptType
      * @return void
      */
-    private void composeOsmScript(OsmScriptType ost) throws Exception {
-
-        noNamespace.QueryType qt = ost.addNewQuery();
+    private void composeOsmScript(UnionType union, String amenity) throws Exception {
+        noNamespace.QueryType qt = union.addNewQuery();
         qt.setType(QueryOSMType.NODE);
 
         HasKvType hkt = qt.addNewHasKv();
         hkt.setK("amenity");
-        hkt.setV("pub");
+        hkt.setV(amenity);
 
         for (Query tempQ : queries) {
             switch (tempQ.getQueryType()) {
@@ -97,11 +103,12 @@ public class OverpassConnector extends Connector {
                     throw new Exception("Invalid attribute type.");
             }
         }
-        ost.addNewPrint();
+
     }
 
     /**
      * Method to add BBOX segment of query to OSM Overpass API OsmScriptType
+     *
      * @param qt to add BBOX to
      * @param tempQ Query
      * @return void
@@ -111,12 +118,12 @@ public class OverpassConnector extends Connector {
         for (Iterator it = tempQ.keySet().iterator(); it.hasNext();) {
             Object key = it.next();
             List<Object> list = (List<Object>) tempQ.get(key);
-            
-            if(checkBbox(list)){
-               bbox.setS((Float) list.get(0));
-               bbox.setW((Float) list.get(1));
-               bbox.setN((Float) list.get(2));
-               bbox.setE((Float) list.get(3));
+
+            if (checkBbox(list)) {
+                bbox.setS((Float) list.get(0));
+                bbox.setW((Float) list.get(1));
+                bbox.setN((Float) list.get(2));
+                bbox.setE((Float) list.get(3));
             } else {
                 throw new Error("Invalid format of BBOX parameter.");
             }
@@ -125,23 +132,28 @@ public class OverpassConnector extends Connector {
 
     /**
      * Method to check for correct format of BBOX
+     *
      * @param list List of Objects
      * @return boolean
      */
-    private boolean checkBbox(List<Object> list){
+    private boolean checkBbox(List<Object> list) {
         boolean isCorrect = false;
-        
-        if(list.size() == 4){
+
+        if (list.size() == 4) {
             isCorrect = true;
             for (int i = 0; i < list.size(); i++) {
-                if(list.get(i) ==  null) isCorrect = false;
+                if (list.get(i) == null) {
+                    isCorrect = false;
+                }
             }
         }
         return isCorrect;
     }
-    
-     /**
-     * Method to add ATTRIBUTE segment of query to OSM Overpass API OsmScriptType
+
+    /**
+     * Method to add ATTRIBUTE segment of query to OSM Overpass API
+     * OsmScriptType
+     *
      * @param qt to add BBOX to
      * @param tempQ Query
      * @return void
@@ -151,7 +163,7 @@ public class OverpassConnector extends Connector {
             Object key = it.next();
             List<Object> list = (List<Object>) tempQ.get(key);
             for (Object obj : list) {
-                if(obj != null){
+                if (obj != null) {
                     HasKvType hkt = qt.addNewHasKv();
                     hkt.setK(obj.toString());
                 }
--- OSMPubGuide-WS/test/de/ifgi/ohbpgiosm/OverpassConnectorTest.java
+++ OSMPubGuide-WS/test/de/ifgi/ohbpgiosm/OverpassConnectorTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.ifgi.ohbpgiosm;
 
 import de.ifgi.ohbpgiosm.util.QueryCreator;
@@ -13,22 +12,24 @@ import java.util.List;
 import java.util.logging.Logger;
 import org.junit.Test;
 
+import java.io.*;
+
 /**
  *
  * @author ondrej
  */
 public class OverpassConnectorTest {
-   
+
     private List<Query> buildQuery() throws ParseException {
         QueryCreator qc = QueryCreator.getInstance();
-        HashMap<Parameter,String> input = new HashMap<>();
+        HashMap<Parameter, String> input = new HashMap<>();
         input.put(Parameter.BBOX, "51.94892,7.5880337,51.973354,7.6595306");
         input.put(Parameter.FILTER, "wheelchair");
-        
+
         List<Query> queries = qc.createQueries(input);
         return queries;
     }
-    
+
     @Test
     public void testConnector() throws ParseException {
         List<Query> queries = this.buildQuery();
@@ -37,6 +38,18 @@ public class OverpassConnectorTest {
             overpassConnector.addQuery(q);
         }
         overpassConnector.sendRequest();
-        System.out.println(overpassConnector.getResponse());
+        //System.out.println(overpassConnector.getResponse());
+        //saveAsFile(overpassConnector, "txt");
+    }
+
+    public void saveAsFile(Connector overpassConnector, String ext) {
+        try {
+            FileWriter fstream = new FileWriter(System.currentTimeMillis() + "out." + ext);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(overpassConnector.getResponse().toString());
+            out.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
