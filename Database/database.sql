54ddb75269dd1f36062dab88371b6464d3cfdd75
froehr <f.roehr@uni-muenster.de>
2014-07-11 13:39:23 +0200

    Added is_open(p_id bigint, startTS timestamp with time zone, stopTS timestamp with time zone) function it checks if a pub is open once between two timestamps

--- Database/database.sql
+++ Database/database.sql
@@ -2,10 +2,10 @@ DROP TABLE closed;
 DROP TABLE opened;
 DROP TABLE temporal_event;
 DROP TABLE pub;
-DROP FUNCTION is_open(p_id integer, ts timestamp with time zone);
-DROP FUNCTION is_open(p_id integer, startTs timestamp with time zone, stopTS timestamp with time zone);
-DROP FUNCTION insert_periodic(event_id integer, state TEXT, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer);
-DROP FUNCTION delete_periodic(event_id integer, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer);
+DROP FUNCTION is_open(p_id bigint, ts timestamp with time zone);
+DROP FUNCTION is_open(p_id bigint, startTs timestamp with time zone, stopTS timestamp with time zone);
+DROP FUNCTION insert_periodic(event_id bigint, state TEXT, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer);
+DROP FUNCTION delete_periodic(event_id bigint, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer);
 
 CREATE TABLE pub(
 pub_ref BIGSERIAL NOT NULL,
@@ -52,16 +52,16 @@ FOREIGN KEY (event_id) REFERENCES temporal_event
 
 /*
 	Function determines if a certain pub is opened at a certain time
-	Param: p_id			INTEGER			OSM Pub ID of the pub
+	Param: p_id			bigint			OSM Pub ID of the pub
 	Param: ts			TIMESTAMP		Checks if the pub is opened at this time
 	Returns: true: pub is opened, false: pub is closed
 */
-CREATE FUNCTION is_open(p_id integer, ts timestamp with time zone) RETURNS BOOLEAN AS $$
+CREATE FUNCTION is_open(p_id bigint, ts timestamp with time zone) RETURNS BOOLEAN AS $$
 	DECLARE result BOOLEAN := FALSE;
 	DECLARE number integer;
 	BEGIN
-	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN opened WHERE pub.pub_ref = p_id AND (ts BETWEEN opened.start_time AND opened.end_time);
-	 IF number != 0
+	  SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN opened WHERE pub.pub_ref = p_id AND (ts BETWEEN opened.start_time AND opened.end_time);
+	  IF number != 0
 	    THEN result := TRUE;
 	 END IF;
 	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN closed WHERE pub.pub_ref = p_id AND (ts BETWEEN closed.start_time AND closed.end_time);
@@ -73,21 +73,21 @@ CREATE FUNCTION is_open(p_id integer, ts timestamp with time zone) RETURNS BOOLE
 $$ LANGUAGE 'plpgsql';
 
 /*
-	Function determines if a certain pub is opened at a certain time
-	Param: p_id			INTEGER							OSM Pub ID of the pub
+	Function determines if a certain pub is opened between two specified timestamps
+	Param: p_id			bigint							OSM Pub ID of the pub
 	Param: startTs		TIMESTAMP with time zone		Starttime of the interval which should be checked
 	Param: stopTs		TIMESTAMP with time zone		Endtime of the interval which should be checked
 	Returns: true: pub is opened, false: pub is closed
 */
-CREATE FUNCTION is_open(p_id integer, startTS timestamp with time zone, stopTS timestamp with time zone) RETURNS BOOLEAN AS $$
+CREATE FUNCTION is_open(p_id bigint, startTS timestamp with time zone, stopTS timestamp with time zone) RETURNS BOOLEAN AS $$
 	DECLARE result BOOLEAN := FALSE;
 	DECLARE number integer;
 	BEGIN
-	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN opened WHERE pub.pub_ref = p_id AND (ts BETWEEN opened.start_time AND opened.end_time);
+	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN opened WHERE pub.pub_ref = p_id AND ((startTS BETWEEN opened.start_time AND opened.end_time) OR (stopTS BETWEEN opened.start_time AND opened.end_time) OR (startTS <= opened.start_time AND stopTS >= opened.end_time));
 	 IF number != 0
 	    THEN result := TRUE;
 	 END IF;
-	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN closed WHERE pub.pub_ref = p_id AND (ts BETWEEN closed.start_time AND closed.end_time);
+	 SELECT COUNT(event_id) INTO number FROM pub NATURAL INNER JOIN temporal_event NATURAL INNER JOIN closed WHERE pub.pub_ref = p_id AND (startTS <= opened.start_time AND stopTS >= opened.end_time);
 	 IF number != 0
 		THEN result := FALSE;
 	 END IF;
@@ -97,14 +97,14 @@ $$ LANGUAGE 'plpgsql';
 
 /*
 	Function creates multiple entries in the "closed" respectively the "opened"-table
-	Param: event_id			INTEGER 		ID of the event for which the periodic opening/closed hours shall be created
+	Param: event_id			bigint 		ID of the event for which the periodic opening/closed hours shall be created
 	Param: state			TEXT 			can be 'opened' or 'closed', decides which table will be used
 	Param: start_date		TIMESTAMP 		Start date of the first occurrence of opening hour/closed hour
 	Param: end_date			TIMESTAMP		End date of the first occurrence of opening hour/closed hour
 	Param: end_periodic		TIMESTAMP		Last occurrence of the periodic event
 	Param: periodic			INTEGER			Timespan between end_periodic and next start of the event
 */
-CREATE FUNCTION insert_periodic(event_id integer, state TEXT, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer) RETURNS void AS $$
+CREATE FUNCTION insert_periodic(event_id bigint, state TEXT, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer) RETURNS void AS $$
     DECLARE start1 integer := EXTRACT(EPOCH FROM start_date);
     DECLARE end1 integer := EXTRACT(EPOCH FROM end_date);
     DECLARE end_periodic INTEGER := EXTRACT(EPOCH FROM end_periodic); 
@@ -127,13 +127,13 @@ $$ LANGUAGE 'plpgsql';
 
 /*
 	Function deletes multiple entries from "closed" and the "opened"-table
-	Param: e_id				INTEGER 		ID of the event for which the periodic opening/closed hours shall be deleted
+	Param: e_id				bigint 		ID of the event for which the periodic opening/closed hours shall be deleted
 	Param: start_date		TIMESTAMP 		Start date of the first occurrence of opening hour/closed hour
 	Param: end_date			TIMESTAMP		End date of the first occurrence of opening hour/closed hour
 	Param: end_periodic		TIMESTAMP		Last occurrence of the periodic event
 	Param: periodic			INTEGER			Timespan between end_periodic and next start of the event
 */
-CREATE FUNCTION delete_periodic(e_id integer, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer) RETURNS void AS $$
+CREATE FUNCTION delete_periodic(e_id bigint, start_date timestamp with time zone, end_date timestamp with time zone, end_periodic timestamp with time zone, periodic integer) RETURNS void AS $$
     DECLARE start1 integer := EXTRACT(EPOCH FROM start_date);
     DECLARE end1 integer := EXTRACT(EPOCH FROM end_date);
     DECLARE end_periodic INTEGER := EXTRACT(EPOCH FROM end_periodic); 
